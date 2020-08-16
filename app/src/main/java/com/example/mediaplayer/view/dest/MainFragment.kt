2c06da0eaf6fc877adabc2d4859aa43b999668bf
kimdanhee <sgoiboy@gmail.com>
2020-08-16 23:26:48 +0900

    [ADD] Dest 화면들 navigation 에 추가 및 설정

---
+++ app/src/main/java/com/example/mediaplayer/view/dest/MainFragment.kt
@@ -0,0 +1,15 @@
+package com.example.mediaplayer.view.dest
+
+import com.example.mediaplayer.R
+import com.example.mediaplayer.base.BaseFragment
+import com.example.mediaplayer.databinding.FragmentMainBinding
+
+class MainFragment: BaseFragment<FragmentMainBinding>(
+  R.layout.fragment_main
+) {
+  override fun FragmentMainBinding.bindingViewData() {
+  }
+
+  override fun FragmentMainBinding.setEventListener() {
+  }
+}
---
+++ app/src/main/java/com/example/mediaplayer/view/dest/MediaPlayerFragment.kt
@@ -0,0 +1,15 @@
+package com.example.mediaplayer.view.dest
+
+import com.example.mediaplayer.R
+import com.example.mediaplayer.base.BaseFragment
+import com.example.mediaplayer.databinding.FragmentMediaPlayerBinding
+
+class MediaPlayerFragment : BaseFragment<FragmentMediaPlayerBinding>(
+  R.layout.fragment_media_player
+) {
+  override fun FragmentMediaPlayerBinding.bindingViewData() {
+  }
+
+  override fun FragmentMediaPlayerBinding.setEventListener() {
+  }
+}
