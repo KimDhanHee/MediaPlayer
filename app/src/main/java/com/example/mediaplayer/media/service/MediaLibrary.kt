5c8f837aec4fada5653af93e5e761a32dd8b429a
Kim Dany <sgoiboy@gmail.com>
2020-08-19 15:36:17 +0900

    [feature] MediaService & MediaLibrary 구현 (#3)
    * [ADD] MediaService & MediaLibrary 구현
    
    * [ADD] MediaService Manifest.xml 에 등록
    
    * [Modify] 변수 word 수정

---
+++ app/src/main/java/com/example/mediaplayer/media/service/MediaLibrary.kt
@@ -0,0 +1,42 @@
+package com.example.mediaplayer.media.service
+
+import android.content.Context
+import android.media.MediaMetadataRetriever
+import android.media.RingtoneManager
+import androidx.media2.common.MediaMetadata
+import androidx.media2.common.UriMediaItem
+
+object MediaLibrary {
+  fun playList(context: Context): List<UriMediaItem> = mutableListOf<UriMediaItem>().apply {
+    val ringtoneManager = RingtoneManager(context).apply { setType(RingtoneManager.TYPE_ALL) }
+    val cursor = ringtoneManager.cursor
+
+    cursor.use {
+      val first = cursor.moveToFirst()
+
+      while (first && cursor.moveToNext()) {
+        val uri = ringtoneManager.getRingtoneUri(cursor.position)
+        val retriever = MediaMetadataRetriever().apply { setDataSource(context, uri) }
+        val title =
+          retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE) ?: "unknown"
+        val duration =
+          retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION).toLong()
+
+        add(
+          UriMediaItem.Builder(uri)
+            .setMetadata(
+              MediaMetadata.Builder()
+                .putString(MediaMetadata.METADATA_KEY_MEDIA_ID, uri.toString())
+                .putString(MediaMetadata.METADATA_KEY_MEDIA_URI, uri.toString())
+                .putString(MediaMetadata.METADATA_KEY_TITLE, title)
+                .putLong(MediaMetadata.METADATA_KEY_DURATION, duration)
+                .putLong(MediaMetadata.METADATA_KEY_BROWSABLE, MediaMetadata.BROWSABLE_TYPE_NONE)
+                .putLong(MediaMetadata.METADATA_KEY_PLAYABLE, 1)
+                .build()
+            )
+            .build()
+        )
+      }
+    }
+  }
+}
